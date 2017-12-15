aa0a32c15a4d11589cc16d8b0a9303535e7d5ada
Mathew Kelly <mjtk.ktjm@gmail.com>
2017-12-15 04:57:39 -0500

    Add basic concurrency to img downloader using threading module.

---
+++ threading/demo_concurrent/thumbnail_maker.py
@@ -0,0 +1,91 @@
+import time
+import os
+import logging
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+import threading
+
+import PIL
+from PIL import Image
+
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
+
+class ThumbnailMakerService(object):
+  """Download and resize a list of images."""
+  def __init__(self, home_dir='.'):
+    self.home_dir = home_dir
+    self.input_dir = self.home_dir + os.path.sep + 'incoming'
+    self.output_dir = self.home_dir + os.path.sep + 'outgoing'
+
+  def download_image(self, url):
+    if not url:
+      return
+
+    logging.info("Downloading {}...".format(url))
+    img_filename = urlparse(url).path.split('/')[-1]
+    urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+    logging.info("Done Downloading {}, took {}.".format(img_filename, end - start))
+
+  def download_images(self, img_url_list):
+    if not img_url_list:
+      return
+
+    os.makedirs(self.input_dir, exist_ok=True)
+
+    logging.info("beginning image downloads...")
+
+    start = time.perf_counter()
+
+    threads = []
+    for url in img_url_list:
+      t = threading.Thread(target=self.download_image, args=(url,))
+      t.start()
+      threads.append(t)
+
+    for t in threads:
+      t.join()
+
+    end = time.perf_counter()
+
+    logging.info("downloaded {} images in {} seconds.".format(len(img_url_list), end - start))
+
+  def perform_resizing(self):
+    if not os.listdir(self.input_dir):
+      return
+    os.makedirs(self.output_dir, exist_ok=True)
+
+    logging.info("beginning image resizing...")
+
+    target_sizes = [32, 64, 200]
+    num_images = len(os.listdir(self.input_dir))
+
+    start = time.perf_counter()
+    for filename in os.listdir(self.input_dir):
+      orig_img = Image.open(self.input_dir + os.path.sep + filename)
+      for basewidth in target_sizes:
+        img = orig_img
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+
+        new_filename = os.path.splitext(filename)[0] + \
+          '_' + str(basewidth) + os.path.splitext(filename)[1]
+        img.save(self.output_dir + os.path.sep + new_filename)
+
+      os.remove(self.input_dir + os.path.sep + filename)
+
+    end = time.perf_counter()
+
+    logging.info("created {} thumbs in {} seconds".format(num_images, end-start))
+
+  def make_thumbnails(self, img_url_list):
+    logging.info("Start make_thumbnails")
+
+    start = time.perf_counter()
+    self.download_images(img_url_list)
+    # self.perform_resizing()
+    end = time.perf_counter()
+
+    logging.info("END make_thumbnails in {} seconds".format(end-start))
+
+
